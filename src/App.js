60428878f5960c8e535451ba397469cbe42822a0
gb-simon <aguilarsimon01@gmail.com>
2021-01-12 19:25:36 -0300

    I added a Link button for the cities section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import "./App.css";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Link } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 class App extends React.Component {
@@ -12,6 +12,7 @@ class App extends React.Component {
           <Route exact path="/">
             <Slider heading="Slider" slides={slideData} />
           </Route>
+
           <Route exact path="/Cordoba">
             <Cordoba />
           </Route>
@@ -24,6 +25,10 @@ class App extends React.Component {
             <Mendoza />
           </Route>
 
+          <Route exact path="/Calafate">
+            <Calafate />
+          </Route>
+
           <Route exact path="/BuenosAires">
             <BuenosAires />
           </Route>
@@ -34,6 +39,122 @@ class App extends React.Component {
         </Switch>
       </BrowserRouter>
     );
+  }
+}
+
+class Cordoba extends React.Component {
+  render() {
+    return (
+      <div className="cites">
+        <h1 className="cities-h1">Cordoba</h1>
+        <img
+          className="img-city"
+          src="https://www.paradavisual.com/wp-content/uploads/2019/11/slider1.jpg"
+          alt="picture_cordoba"
+        ></img>
+        <p>
+          Córdoba is a city in central Argentina, in the foothills of the
+          Sierras Chicas on the Suquía River, about 700 km (435 mi) northwest of
+          the Buenos Aires. It is the capital of Córdoba Province and the second
+          most populous city in Argentina after Buenos Aires, with about 1.3
+          million inhabitants according to the 2010 census. It was founded on 6
+          July 1573 by Jerónimo Luis de Cabrera, who named it after Córdoba,
+          Spain. It was one of the early Spanish colonial capitals of the region
+          that is now Argentina <a href="wikipedia.com">More Information</a>{" "}
+        </p>
+        <h2>Historia</h2>
+        <img
+          className="img-city"
+          src="https://upload.wikimedia.org/wikipedia/commons/1/17/Avenida_Olmos%2C_C%C3%B3rdoba_%281943%29.jpg"
+          alt="Avenida Olmos, Córdoba (1943)"
+        ></img>
+        <p>
+          Córdoba is a city in central Argentina, in the foothills of the
+          Sierras Chicas on the Suquía River, about 700 km (435 mi) northwest of
+          the Buenos Aires. It is the capital of Córdoba Province and the second
+          most populous city in Argentina after Buenos Aires, with about 1.3
+          million inhabitants according to the 2010 census. It was founded on 6
+          July 1573 by Jerónimo Luis de Cabrera, who named it after Córdoba,
+          Spain. It was one of the early Spanish colonial capitals of the region
+          that is now Argentina
+        </p>
+        <p>
+          In 1776, King Carlos III created the Viceroyalty of the Río de la
+          Plata, in which Córdoba stays in 1785 as the Government Intendency of
+          Córdoba, including the current territories of the provinces of
+          Córdoba, La Rioja and the region of Cuyo. According to the 1760
+          census, the population of the city was 22,000 inhabitants. During the
+          May Revolution in 1810, the widespread opinion of the most notable
+          citizens was of continuing respecting the orders of Fernando VII,
+          attitude assumed by the local authorities, which led to the Liniers
+          Counter-revolution. This position was not shared by the Dean Gregorio
+          Funes, who was adhering to the revolutionary ideas, beside supporting
+          contact with Manuel Belgrano and Juan José Castelli.
+        </p>
+        <h2>Geography</h2>
+        <p>
+          The city's geographic location is 31°25′S 64°11′W, taking as a point
+          of reference San Martín Square in downtown Córdoba. The relative
+          location of the municipal common land, is in the south hemisphere of
+          the globe, to the south of the South American subcontinent, in the
+          geographical centre – west of Argentina and of the province of
+          Córdoba; to a distance of 702 km (436 mi) from Buenos Aires and 401 km
+          (249 mi) from the city of Rosario
+        </p>
+        <h2>Demographics</h2>
+        Population: around 1,3m
+        <h3>Ethnicity</h3>
+        <p>
+          The largest ethnic groups in Córdoba are Italians/Italian Argentine
+          and Spaniards/Spanish Argentine (mostly Galicians and Basques/Basque
+          Argentine). Waves of immigrants from other European countries arrived
+          in the late 19th and early 20th centuries. From the rest of Western
+          Europe came immigrants from Switzerland, Germany, United Kingdom,
+          Ireland and Scandinavia (especially Sweden). Eastern Europeans also
+          arrived from nations such as Croatia, Poland, Hungary, Russia,
+          Romania, Ukraine, Armenia and the Balkans (especially Greece, Serbia
+          and Montenegro). By the 1910s, 43 percent of the city population was
+          non-native Argentine after immigration rates peaked.[13][14] Important
+          Lebanese, Georgian, Syrian and Armenian communities have had a
+          significant presence in commerce and civic life since the beginning of
+          the 20th century.
+        </p>
+        <h2>Urban structure</h2>
+        <h2>Economy</h2>
+        <h2>Culture</h2>
+        <h2>Gallery</h2>
+      </div>
+    );
+  }
+}
+
+class BuenosAires extends React.Component {
+  render() {
+    return <div>This is Buenos Aires</div>;
+  }
+}
+
+class Aconcagua extends React.Component {
+  render() {
+    return <div>This is Aconcagua</div>;
+  }
+}
+
+class Calafate extends React.Component {
+  render() {
+    return <div>This is El Calafate</div>;
+  }
+}
+
+class Mendoza extends React.Component {
+  render() {
+    return <div>This is Mendoza</div>;
+  }
+}
+
+class Humahuaca extends React.Component {
+  render() {
+    return <div>This is Humahuaca</div>;
   }
 }
 
@@ -61,7 +182,7 @@ const slideData = [
   },
   {
     index: 3,
-    headline: "El-Calafate",
+    headline: "Calafate",
     button: "For Your Current Mood",
     src:
       "https://images.unsplash.com/photo-1465917566611-efba2904dd8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2255&q=80",
@@ -81,62 +202,6 @@ const slideData = [
       "https://images.unsplash.com/photo-1545889238-ae8ff5ab582f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2249&q=80",
   },
 ];
-
-class Cordoba extends React.Component {
-  render() {
-    return (
-      <div className="cites">
-        <div className="first-city">
-          <h1 className="cities-h1">Cordoba</h1>
-          <img
-            class="img-city"
-            src="https://docrwwop59xq2.cloudfront.net/uploads/2017/01/cordoba-argentina-2.jpg"
-            alt="picture_cordoba"
-          ></img>
-          <p>
-            Cordoba is a city in central Argentina, in the foothills of the
-            Sierras Chicas on the Suquía River, about 700 km (435 mi) northwest
-            of the Buenos Aires. It is the capital of Córdoba Province and the
-            second most populous city in Argentina after Buenos Aires, with
-            about 1.3 million inhabitants according to the 2010 census. It was
-            founded on 6 July 1573 by Jerónimo Luis de Cabrera, who named it
-            after Córdoba, Spain. It was one of the first Spanish colonial
-            capitals of the region that is now Argentina (the oldest city is
-            Santiago del Estero, founded in 1553). The National University of
-            Córdoba is the oldest university of the country and the seventh to
-            be inaugurated in Spanish America. It was founded in 1613 by the
-            Jesuit Order. Because of this, Córdoba earned the nickname La Docta
-            ("the learned").<a href="wikipedia.com">More Information</a>
-          </p>
-        </div>
-      </div>
-    );
-  }
-}
-
-class BuenosAires extends React.Component {
-  render() {
-    return <div>This is Buenos Aires</div>;
-  }
-}
-
-class Aconcagua extends React.Component {
-  render() {
-    return <div>This is Aconcagua</div>;
-  }
-}
-
-class Mendoza extends React.Component {
-  render() {
-    return <div>This is Mendoza</div>;
-  }
-}
-
-class Humahuaca extends React.Component {
-  render() {
-    return <div>This is Humahuaca</div>;
-  }
-}
 
 // Slide
 
@@ -206,7 +271,9 @@ class Slide extends React.Component {
 
         <article className="slide__content">
           <h2 className="slide__headline">{headline}</h2>
-          <button className="slide__action btn">{button}</button>
+          <Link to={headline} type="submit" className="btn">
+            {button}
+          </Link>
         </article>
       </li>
     );
@@ -263,7 +330,7 @@ class Slider extends React.Component {
       <div>
         <div className="slider" aria-labelledby={headingId}>
           <ul className="slider__wrapper" style={wrapperTransform}>
-            <h3 id={headingId} class="visuallyhidden">
+            <h3 id={headingId} className="visuallyhidden">
               {heading}
             </h3>
 
@@ -274,7 +341,6 @@ class Slider extends React.Component {
                   slide={slide}
                   current={current}
                   handleSlideClick={this.handleSlideClick}
-                  // text={city_information}
                 />
               );
             })}
